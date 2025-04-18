9caa5696f5df5e845bbc80d8234de05e16b5ccd4
sankarganesh-r <sankarrathinam27@gmail.com>
2025-04-18 16:09:37 +0530

    Simplify the code

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -7,26 +7,16 @@ public class Dice : MonoBehaviour
 {
     Animator animator;
     public string[] animationTriggers;
-    int diceValue;
-    Action mainCallBack;
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    public void AnimationTrigger(int diceValue, Action callback)
-    {
-        mainCallBack = callback;
-        this.diceValue = diceValue;
+    public IEnumerator AnimationLandedTrigger(int diceValue, Action onComplete){
         animator.SetTrigger("Rolling");
-        StartCoroutine(AnimationLandedTrigger());
-    }
-
-    IEnumerator AnimationLandedTrigger(){
-
         yield return new WaitForSeconds(1f);
         animator.SetTrigger(animationTriggers[diceValue-1]);
         yield return new WaitForSeconds(1f);
-        mainCallBack();
+        onComplete?.Invoke();
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour
     public Button rollDiceButton;
     public Dice dice1, dice2;
     int total;
+    int animationsCompleted = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -21,22 +22,26 @@ public class GameManager : MonoBehaviour
     }
 
     void RollDice(){
-
-        int die1= Random.Range(1,7);
-        dice1.AnimationTrigger(die1, Result);
-        int die2=Random.Range(1,7);
-        dice2.AnimationTrigger(die2, Result);
+        resultText.text = "Rolling..";
+        int die1 = Random.Range(1,7);
+        StartCoroutine(dice1.AnimationLandedTrigger(die1, Result)); 
+        int die2 = Random.Range(1,7);
+        StartCoroutine(dice2.AnimationLandedTrigger(die2, Result));
         total = die1 + die2;
-        Debug.Log("Total "+total);
+        Debug.Log("Total "+ total);
     }
 
     void Result(){
 
+        animationsCompleted++;
+        if(animationsCompleted < 2) return;
+        animationsCompleted = 0;
+
         if(total==1 || total == 7)
-            resultText.text = "Your Total is "+total + "\nYou Win";
+            resultText.text = $"Your Total is {total}\nYou Win!!!!";
         else if(total==2 || total ==3 || total == 12)
-            resultText.text = "Your Total is "+total + "\nYou Lose";
+            resultText.text = $"Your Total is {total}\nYou Lose....";
         else 
-            resultText.text = "Your Total is "+total + "\nRoll Again";
+            resultText.text = $"Your Total is {total}\nRoll Again...";
     }
 }
